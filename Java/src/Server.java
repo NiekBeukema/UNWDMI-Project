bb2f7b2dcc9c34551107ecb62e0672120f2772c5
Kraylor <gameking.mohm@hotmail.com>
2017-01-24 14:19:47 +0100

    adjusted Server to read xml input (no parser) and write output from array (plain text) added testfile for server output testing

--- Java/src/Server.java
+++ Java/src/Server.java
@@ -1,25 +1,50 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
 	
 	public static void main(String args[]) throws IOException {
 		final int portNumber = 7789;
+		String filename = "src\\testfile.txt";
+		ArrayList<String> array = new ArrayList<String>();
+		
 		System.out.println("Creating server socket on port " + portNumber);
 		ServerSocket serverSocket = new ServerSocket(portNumber);
 		
 		while (true) {
+			//accept socket connection
 			Socket socket = serverSocket.accept();
-
+			String str = "";
+			//get inputstream
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String str = br.readLine();
-
+			
+			//read input data
+			while (!str.contains("</MEASUREMENT>")) {
+				str = br.readLine();
+				array.add(str);
+			}			
+			
+			//close socket for new connection
 			socket.close();
-
-			System.out.println("Just said hello to:" + str);
+			
+			//write data to tekstfile from array
+			try(BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))){
+			    for (String data : array){
+			    	bw.write(data);
+			    }
+			    bw.newLine();
+			}
+			
+			System.out.println(array);
+			
+			array.clear();
 		}
 	}
 }
