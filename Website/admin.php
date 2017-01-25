2b2e1e18fd862cd43043ce1dfb353e098e7ea733
Niek <n.beukema@st.hanze.nl>
2017-01-25 14:10:54 +0100

    ReAdded style, fixed export buttons.

--- Website/admin.php
+++ Website/admin.php
@@ -1,170 +1,233 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: David
- * Date: 23-1-2017
- * Time: 13:54
- */
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>IICA</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-    <title>Administration | IICA</title>
+    <link href="vendors/bower_components/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet" type="text/css"/>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="js/datatables/media/css/jquery.dataTables.min.css" rel="stylesheet">
-    <link href="https://hencework.com/theme/kenny/vendors/bower_components/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/style1.css" rel="stylesheet">
+    <!-- Data table CSS -->
+    <link href="vendors/bower_components/datatables/media/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+    <link href="vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.css" rel="stylesheet" type="text/css">
 
-  <body>
+    <!-- Custom CSS -->
+    <link href="dist/css/style.css" rel="stylesheet" type="text/css">
+</head>
 
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-          <div class="navbar-header">
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-              </button>
-              <a class="navbar-brand" href="#">IICA</a>
-          </div>
-          <div id="navbar" class="collapse navbar-collapse">
-              <ul class="nav navbar-nav">
-                  <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Oceania <span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                          <li><a href="#">Realtime</a></li>
-                          <li role="separator" class="divider"></li>
-                          <li><a href="#">History</a></li>
-                      </ul>
-                  </li>
-                  <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Argentina <span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                          <li><a href="#">Realtime</a></li>
-                          <li role="separator" class="divider"></li>
-                          <li><a href="#">History</a></li>
+<body>
+<!-- Preloader -->
+<div class="preloader-it">
+    <div class="la-anim-1"></div>
+</div>
+<!-- /Preloader -->
+<div class="wrapper">
+    <!-- Top Menu Items -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <a id="toggle_nav_btn" class="toggle-left-nav-btn inline-block mr-20 pull-left" href="javascript:void(0);"><i class="fa fa-bars"></i></a>
+        <a href="index.html"><img class="brand-img pull-left" src="logo.png" alt="brand"/></a>
+        <ul class="nav navbar-right top-nav pull-right">
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle pr-0" data-toggle="dropdown">Kermit de Kikker</a>
+                <ul class="dropdown-menu user-auth-dropdown" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-envelope"></i> Administration</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <!-- /Top Menu Items -->
 
-                      </ul>
-                  </li>
-              </ul>
-              <ul class="nav navbar-nav navbar-right">
-                  <li class="dropdown">
-                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Kermit De Kikker<span class="caret"></span></a>
-                      <ul class="dropdown-menu">
-                          <li><a href="admin.php">Administration</a></li>
-                          <li><a href="#">Another action</a></li>
-                      </ul>
-                  </li>
-              </ul>
-          </div><!--/.nav-collapse -->
-      </div>
-  </nav>
-
-  <div class="container">
-      <div class="row">
-          <div class="col-sm-12">
-              <div class="panel panel-default card-view">
-                  <div class="panel-heading">
-                      <div class="pull-left">
-                          <h6 class="panel-title txt-dark">Add / Remove Users</h6>
-                      </div>
-                      <div class="clearfix"></div>
-                  </div>
-                  <div class="panel-wrapper collapse in">
-                      <div class="panel-body">
-                          <form action="#">
-                              <div class="form-group">
-                                <label class="control-label mb-10 text-left">Username</label>
-                                <input type="text" name="username" class="form-control" placeholder="Username"/>
-                              </div>
-                              <div class="form-group">
-                                  <div class="checkbox checkbox-primary">
-                                      <input type="checkbox" id="admin"/>
-                                      <label for="admin">Is this user an administrator?</label>
-                                  </div>
-                              </div>
-                              <div class="form-group">
-                                <h6>A random password will be generated</h6>
-                              </div>
-                              <div class="form-group mb-0">
-                                  <div class="col-sm-2">
-                                      <button class="btn btn-success">
-                                          <span class="btn-text">Submit</span>
-                                      </button>
-                                  </div>
-                              </div>
-                          </form>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-sm-12">
-              <div class="panel panel-default card-view">
-                  <div class="panel-heading">
-                      <div class="pull-left">
-                          <h6 class="panel-title txt-dark">Add / Remove Admin Privileges</h6>
-                      </div>
-                      <div class="clearfix"></div>
-                  </div>
-                  <div class="panel-wrapper collapse in">
-                      <div class="panel-body">
-                          <form action="#">
-                              <div class="form-group">
-                                  <label class="control-label mb-10">Select box</label>
-                                  <select class="selectpicker" data-style="form-control">
-                                      <option>Mustard</option>
-                                      <option>Ketchup</option>
-                                      <option>Relish</option>
-                                  </select>
-                          </form>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-
-  </div><!-- /.container -->
+    <!-- Left Sidebar Menu -->
+    <div class="fixed-sidebar-left">
+        <ul class="nav navbar-nav side-nav nicescroll-bar">
+            <li>
+                <a  class="active" href="javascript:void(0);" data-toggle="collapse" data-target="#dashboard_dr"><i class="icon-picture mr-10"></i>Oceania <span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
+                <ul id="dashboard_dr" class="collapse collapse-level-1">
+                    <li>
+                        <a class="active" href="#">Realtime</a>
+                    </li>
+                    <li>
+                        <a href="#">History</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="javascript:void(0);" data-toggle="collapse" data-target="#ecom_dr"><i class="icon-picture mr-10"></i>Argentina<span class="pull-right"><i class="fa fa-fw fa-angle-down"></i></span></a>
+                <ul id="ecom_dr" class="collapse collapse-level-1">
+                    <li>
+                        <a href="#">Realtime</a>
+                    </li>
+                    <li>
+                        <a href="#">History</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    <!-- /Left Sidebar Menu -->
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-  <!-- Data table JavaScript -->
-  <script src="js/datatables/media/js/jquery.dataTables.min.js"></script>
-  <script src="js/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-  <script src="js/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <!-- Main Content -->
+    <div class="page-wrapper">
+        <div class="container-fluid">
 
-  <script src="js/datatables.net-buttons/js/buttons.html5.min.js"></script>
-  <script src="js/datatables.net-buttons/js/buttons.print.min.js"></script>
-  <script src="https://hencework.com/theme/kenny/vendors/bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-  <script src="js/export-table-data.js"></script>
-  <script src="js/jszip/dist/jszip.min.js"></script>
-  <script src="js/pdfmake/build/pdfmake.min.js"></script>
-  <script src="js/pdfmake/build/vfs_fonts.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
-  </body>
+            <!-- Title -->
+            <div class="row heading-bg  bg-red">
+                <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                    <h5 class="txt-light">User Administration</h5>
+                </div>
+            </div>
+            <!-- /Title -->
+
+            <!-- Row -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel panel-default card-view">
+                            <div class="panel-heading">
+                                <div class="pull-left">
+                                    <h6 class="panel-title txt-dark">Add / Remove Users</h6>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+                                    <form action="#">
+                                        <div class="form-group">
+                                            <label class="control-label mb-10 text-left">Username</label>
+                                            <input type="text" name="username" class="form-control" placeholder="Username"/>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="checkbox checkbox-primary">
+                                                <input type="checkbox" id="admin"/>
+                                                <label for="admin">Is this user an administrator?</label>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <h6>A random password will be generated</h6>
+                                        </div>
+                                        <div class="form-group mb-0">
+                                            <div class="col-sm-2">
+                                                <button class="btn btn-success">
+                                                    <span class="btn-text">Submit</span>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel panel-default card-view">
+                            <div class="panel-heading">
+                                <div class="pull-left">
+                                    <h6 class="panel-title txt-dark">Add / Remove Admin Privileges</h6>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="panel-wrapper collapse in">
+                                <div class="panel-body">
+                                    <form action="#">
+                                        <div class="form-group">
+                                            <label class="control-label mb-10">Select box</label>
+                                            <select class="selectpicker" data-style="form-control">
+                                                <option>Mustard</option>
+                                                <option>Ketchup</option>
+                                                <option>Relish</option>
+                                            </select>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div><!-- /.container -->
+            <!-- Row -->
+        </div>
+        <!-- Footer -->
+        <footer class="footer container-fluid pl-30 pr-30">
+            <div class="row">
+                <div class="col-sm-5">
+                    <a href="index.html" class="brand mr-30"><img src="logo.png" alt="logo"/></a>
+                    <ul class="footer-link nav navbar-nav">
+                        <li class="logo-footer"><a href="#">help</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-7 text-right">
+                    <p>2016 &copy; IICA JMDR-IT</p>
+                </div>
+            </div>
+        </footer>
+        <!-- /Footer -->
+
+    </div>
+    <!-- /Main Content -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- JavaScript -->
+
+<!-- jQuery -->
+<script src="vendors/bower_components/jquery/dist/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+<!-- Data table JavaScript -->
+<script src="vendors/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+<script src="vendors/bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="vendors/bower_components/datatables.net-buttons/js/buttons.flash.min.js"></script>
+<script src="vendors/bower_components/jszip/dist/jszip.min.js"></script>
+<script src="vendors/bower_components/pdfmake/build/pdfmake.min.js"></script>
+<script src="vendors/bower_components/pdfmake/build/vfs_fonts.js"></script>
+
+<script src="vendors/bower_components/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="vendors/bower_components/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="js/export-table-data.js"></script>
+<!-- Bootstrap Select JavaScript -->
+<script src="vendors/bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+
+<!-- Slimscroll JavaScript -->
+<script src="dist/js/jquery.slimscroll.js"></script>
+
+<!-- Progressbar Animation JavaScript -->
+<script src="vendors/bower_components/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="vendors/bower_components/Counter-Up/jquery.counterup.min.js"></script>
+
+<!-- Fancy Dropdown JS -->
+<script src="dist/js/dropdown-bootstrap-extended.js"></script>
+
+<!-- Sparkline JavaScript -->
+<script src="vendors/jquery.sparkline/dist/jquery.sparkline.min.js"></script>
+
+<!-- ChartJS JavaScript -->
+<script src="vendors/chart.js/Chart.min.js"></script>
+
+<!-- Init JavaScript -->
+<script src="dist/js/init.js"></script>
+
+</body>
+
 </html>
