366fe394b4c5455e5eac552f2b8c6f20f7874e86
Niek <n.beukema@st.hanze.nl>
2017-01-31 15:02:56 +0100

    ReAdded style, fixed export buttons.

--- Website/dist/js/export-table-data.js
+++ Website/dist/js/export-table-data.js
@@ -28,3 +28,21 @@ $(document).ready(function() {
         }
     } );
 } );
+
+function RefreshGraph() {
+    $.ajax({
+        url : 'config/datatable.php',
+        type : 'POST',
+        dataType : 'json',
+        success : function (result) {
+            var getTime = new Date();
+            datasetgpu1.update({x: getTime.getTime(), y: result['gpu1']});
+            datasetcpu1.update({x: getTime.getTime(), y: result['cpu1']});
+            $("#gputempbadge").text(result['gpu1']);
+            $("#cputempbadge").text(result['cpu']);
+        },
+        error : function (obj, ovj, error) {
+            reportError(error);
+        }
+    })
+}
